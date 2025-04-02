e6a8b677ea1f6db5a131b6e7e96b2f95d9f18ea4
ThanhTris <thanhtri005@gmail.com>
2025-04-03 00:29:42 +0700

    chinh lai layout

--- frontend-admin/src/app/books/[id]/page.js
+++ frontend-admin/src/app/books/[id]/page.js
@@ -1,7 +1,32 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import useSidebarStore from "@/store/sideBarStore";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { Button } from "@/app/components/ui/button";
 
 export default function EditBook() {
+  const { isSidebarOpen, toggleSidebar } = useSidebarStore();
+
+  useEffect(() => {
+    console.log("Sidebar Toggled. isSidebarOpen:", isSidebarOpen);
+  }, [isSidebarOpen]);
+
   return (
-    <div>Edit Book</div>
-  )
+    <div className="flex bg-[#F4F7FE] min-h-screen">
+      {/* Sidebar */}
+      <Sidebar />
+      <div
+        className={`flex-1 px-6 py-4 w-full transition-all duration-300 ${
+          isSidebarOpen ? "md:ml-6" : "md:ml-0"
+        }`}
+      >
+        <div className="text-gray-500">
+          trianajnaskjafojsdoianfojasnfjanfno
+        </div>
+        <Button className="p-6 mt-4" onClick={toggleSidebar}>
+          Change Sidebar State
+        </Button>
+      </div>
+    </div>
+  );
 }
