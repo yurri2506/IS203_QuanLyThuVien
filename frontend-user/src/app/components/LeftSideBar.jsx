4f5754a769581e2ac557918d29b3929011318d59
rosy179 <lerosy179@gmail.com>
2025-04-02 19:05:14 +0700

    fix lai menu

--- frontend-user/src/app/components/LeftSideBar.jsx
+++ frontend-user/src/app/components/LeftSideBar.jsx
@@ -11,13 +11,13 @@ import {
   LogOut,
   User,
 } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
-
 
 const LeftSideBar = () => {
   const { isSidebarOpen, toggleSidebar } = useSidebarStore();
   const router = useRouter();
+  const pathname = usePathname();
 
   const handleNavigation = (path, item) => {
     router.push(path);
@@ -34,34 +34,74 @@ const LeftSideBar = () => {
           : " -translate-x-full"
       } ${isSidebarOpen ? "md:hidden" : ""} md:bg-transparent md:shadow-none`}
     >
-      <div className="flex flex-col h-full overflow-y-auto">
+      <div className="flex flex-col h-[630px] overflow-y-auto bg-white rounded-lg">
         {/* navigation menu yaha pr */}
         <nav className="space-y-4 flex-grow">
-          <Button variant="ghost" className="full justify-start"  onClick={() => handleNavigation("/user-profile")}>
+          <Button
+            variant="ghost"
+            onClick={() => handleNavigation("/user-profile")}
+            className={`full justify-start w-full ${
+              pathname === "/user-profile"
+                ? "bg-blue-400 text-white hover:bg-blue-400"
+                : ""
+            }`}
+          >
             <User className="mr-4" /> Hồ sơ cá nhân
           </Button>
 
-          <Button variant="ghost" className="full justify-start" onClick={() => handleNavigation("/borrowed-books")}>
+          <Button
+            variant="ghost"
+            onClick={() => handleNavigation("/borrowed-books")}
+            className={`full justify-start w-full ${
+              pathname === "/borrowed-books"
+                ? "bg-blue-400 text-white hover:bg-blue-400"
+                : ""
+            }`}
+          >
             <BookText className="mr-4" /> Sách đang mượn
           </Button>
 
-          <Button variant="ghost" className="full justify-start" onClick={() => handleNavigation("/overdue-books")}>
+          <Button
+            variant="ghost"
+            onClick={() => handleNavigation("/overdue-books")}
+            className={`full justify-start w-full ${
+              pathname === "/overdue-books"
+                ? "bg-blue-400 text-white hover:bg-blue-400"
+                : ""
+            }`}
+          >
             <CircleAlert className="mr-4" /> Sách quá hạn
           </Button>
-          <Button variant="ghost" className="full justify-start" onClick={() => handleNavigation("/change-password")}>
+          <Button
+            variant="ghost"
+            onClick={() => handleNavigation("/change-password")}
+            className={`full justify-start w-full ${
+              pathname === "/change-password"
+                ? "bg-blue-400 text-white hover:bg-blue-400"
+                : ""
+            }`}
+          >
             <LockKeyhole className="mr-4" /> Đổi mật khẩu
           </Button>
-          <Button variant="ghost" className="full justify-start" onClick={() => handleNavigation("/notification")}>
+          <Button
+            variant="ghost"
+            onClick={() => handleNavigation("/notification")}
+            className={`full justify-start w-full ${
+              pathname === "/notification"
+                ? "bg-blue-400 text-white hover:bg-blue-400"
+                : ""
+            }`}
+          >
             <Bell className="mr-4" /> Thông báo
           </Button>
         </nav>
 
         {/* footer section */}
 
-        <div className="mb-16">
+        <div className="mb-5">
           <Separator className="my-4" />
           <div className="text-xs text-muted-foreground space-y-1">
-            <Button variant="ghost" className="cursor-pointer -ml-4 ">
+            <Button variant="ghost" className="cursor-pointer ml-10 ">
               <LogOut />{" "}
               <span className="ml-2 font-bold text-md">Đăng xuất</span>
             </Button>
