04fb6cba26e3012454678823ef13828e31a7ae0b
yurri2506 <22520590@gm.uit.edu.vn>
2025-04-13 15:33:27 +0700

    CRUD book

---
+++ backend2/library/src/main/java/com/library_web/library/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.library_web.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+            .httpBasic(httpBasic -> {});
+        return http.build();
+    }
+}
+
