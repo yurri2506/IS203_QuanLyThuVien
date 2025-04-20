5e3e310087d355f9aed79ad57a4228aa5f01ebd4
ThanhTris <thanhtri005@gmail.com>
2025-04-20 22:06:13 +0700

    Sửa UserRepository

--- backend2/library/src/main/java/com/library_web/library/repository/UserRepository.java
+++ backend2/library/src/main/java/com/library_web/library/repository/UserRepository.java
@@ -6,12 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username); // Thêm phương thức tìm kiếm theo tên đăng nhập
-    Optional<User> findById(Long id); // Thêm phương thức tìm kiếm theo ID
+    Optional<User> findByUsername(String username); // Tìm kiếm theo tên đăng nhập
+    Optional<User> findByEmail(String email); // Tìm kiếm theo email
+    Optional<User> findByResetToken(String resetToken); // Tìm kiếm theo token đặt lại mật khẩu
     Optional<User> findByProviderId(String providerId); // Tìm kiếm theo ID của Google/Facebook
-    Optional<User> findByProvider(String provider); // Tìm kiếm theo provider (Google, Facebook)
-    Optional<User> findByName(String name); // Tìm kiếm theo tên người dùng
-    
-    Optional<User> findByEmail(String email);
-    Optional<User> findByResetToken(String resetToken);
 }
