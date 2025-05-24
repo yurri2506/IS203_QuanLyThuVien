c75a4641d3a8b02cdf0374eb965b490329ce3412
ThanhTris <thanhtri005@gmail.com>
2025-05-25 06:13:58 +0700

    api lấy sách 7 ngày thành công

--- backend2/library/src/main/java/com/library_web/library/repository/BorrowCardRepository.java
+++ backend2/library/src/main/java/com/library_web/library/repository/BorrowCardRepository.java
@@ -1,9 +1,13 @@
 package com.library_web.library.repository;
 
 import com.library_web.library.model.BorrowCard;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BorrowCardRepository extends JpaRepository<BorrowCard, Long> {
   List<BorrowCard> findByUserId(Long userId);
+
+  List<BorrowCard> findByGetBookDateAfter(LocalDateTime date);
 }
